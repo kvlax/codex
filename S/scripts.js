3f902d05a9ee6ec60da0d99b0c0188a509333373
kvlax <154732042+kvlax@users.noreply.github.com>
2024-01-29 16:39:38 +0100

    Create scripts.js

---
+++ S/scripts.js
@@ -0,0 +1,11 @@
+// scripts.js
+function signup() {
+    const username = document.getElementById('username').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    // Perform signup logic (you can add AJAX requests or other actions here)
+    
+    // Redirect to a success page or perform further actions as needed
+    // window.location.href = '/success.html';
+}
